0d97986bb87f538dedca3acd5b0ed282aee22a3d
Taslan Graham <gtaslan1@gmail.com>
2024-08-16 12:53:36 -0500

    will this fail

--- cypress/tests/functional/Citations.cy.js
+++ cypress/tests/functional/Citations.cy.js
@@ -4,13 +4,13 @@ describe('Scopus/Crossref Plugin tests', function () {
 	it('Disable Scopus/Crossref Plugin', function () {
 		cy.login('admin', 'admin', 'publicknowledge');
 		cy.get('nav[class="app__nav"] a:contains("Website")').click();
-		cy.get('button[id="plugins-button"]').click();
+		cy.get('button[id="plugins-buttn"]').click();
 		// disable plugin if enabled
 		cy.get('input[id^="select-cell-citationsplugin-enabled"]')
 			.then($btn => {
 				if ($btn.attr('checked') === 'checked') {
 					cy.get('input[id^="select-cell-citationsplugin-enabled"]').click();
-					cy.get('div[class*="pkp_modal_panel"] button[class*="pkpModalConfirmButton"]').click();
+					cy.get('div[class*="pkp_modal_panel"] button[class*="pkpModalConfirmButton"]').should('exist').click();
 					cy.get('div:contains(\'The plugin "Scopus/Crossref Plugin" has been disabled.\')');
 				}
 			});
